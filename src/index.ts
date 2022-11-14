946ee7a7648e48386b6fc49b6705d0c239d44521
KillDozerX2 <himanshupant127@gmail.com>
2022-11-14 09:48:04 +0530

    feat: :sparkles: added new index.ts file as the starter for the plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const myPackage = (taco = ""): string => `${taco} from my package`;
+import Serverless from "serverless";
+
+interface ServerlessPluginOptions {
+  globalOptions?: boolean;
+}
+class ServerlessPlugin {
+  serverless: Serverless;
+  options: ServerlessPluginOptions;
+  constructor(serverless: Serverless, options: ServerlessPluginOptions) {
+    this.serverless = serverless;
+    this.options = options;
+  }
+}
+
+module.exports = ServerlessPlugin;
